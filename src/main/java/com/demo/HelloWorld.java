d97dd0334209cd9778f160e49d021d43d5dbb320
salmaaan352@gmail.com <salman352@AsusTech.localdomain>
2026-05-01 05:20:26 +0000

    1 more change

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -10,7 +10,7 @@ import java.net.InetSocketAddress;
 public class HelloWorld {
 
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
 
         server.createContext("/", new HttpHandler() {
             public void handle(HttpExchange exchange) {
