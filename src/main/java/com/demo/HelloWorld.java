d8deb08b7cbd130ad76cdc3b52a022a37329e918
salmaaan352@gmail.com <salman352@AsusTech.localdomain>
2026-05-01 05:17:50 +0000

    made chages in java programmer

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -1,6 +1,33 @@
 package com.demo;
- public class HelloWorld{
-    public static void main (String[] args){
-        System.out.println("Hello from jenkins CI pipeline");
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HelloWorld {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) {
+                try {
+                    String response = "Hello from Jenkins CI pipeline!";
+                    exchange.sendResponseHeaders(200, response.length());
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server started on port 8080");
     }
- }
+}
