bfe9ebb64b5b3ab0367dde4fd8e9ad3fd6d0daee
salmaaan352@gmail.com <salman352@AsusTech.localdomain>
2026-05-08 11:38:07 +0000

    Maded changes in java file

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -10,12 +10,29 @@ import java.net.InetSocketAddress;
 public class HelloWorld {
 
     public static void main(String[] args) throws Exception {
+
+        String version ="version 2- Deployed via jenkins CI/CD";
+        String DeployTime =LocalDateTime.now().toString();
+
+        //Bind to all interfaces so it is accessibly externally
+    
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         server.createContext("/", new HttpHandler() {
             public void handle(HttpExchange exchange) {
                 try {
-                    String response = "Hello from Jenkins CI pipeline!";
+                    String response = 
+                    "=======  jenkins CI/CD Demo Applications ======\n\n" +
+                    "Hello from jenkins Pipeline! \n\n" +
+                    "Application version: " + version + "\n" +
+                    "Deployment Time :" + deployTime + "\n" +
+                    "server Hostname   : " + hostname + "\n" +
+                    "Request Details \n" + 
+                    "-------------------- \n"+
+                    "Requested url         : " +  requestURI + "\n" +
+                    "User Agent            :" + userAgent + "\n" ;
+                
+
                     exchange.sendResponseHeaders(200, response.length());
                     OutputStream os = exchange.getResponseBody();
                     os.write(response.getBytes());
@@ -29,5 +46,8 @@ public class HelloWorld {
         server.start();
 
         System.out.println("Server started on port 8080");
+        System.out.println("Appliction version: " + version);
+        System.out.println("Deployment Time: " + deployTime);
+        System.out.println("Hostname:  " + hostname);
     }
 }
